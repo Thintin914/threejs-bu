86800674ab9d93e8d622ab58c72b7988bc7d7b9c
TimHTL02 <timtszlok@gmail.com>
2024-04-10 04:17:12 +0800

    arena

--- threejs-bu/src/scenes/Game.tsx
+++ threejs-bu/src/scenes/Game.tsx
@@ -12,38 +12,44 @@ import { useParams } from 'react-router-dom';
 import { useAccountStore } from '../utils/zustand/useAccountStore';
 import * as CANNON from 'cannon-es'
 
-export function Game(){
+export function Game() {
 
-    const {id} = useParams();
-    const {setFading} = useTransitionStore();
-    const {account, skin} = useAccountStore();
+    const { id } = useParams();
+    const { setFading } = useTransitionStore();
+    const { account, skin } = useAccountStore();
 
     const container = useRef<HTMLDivElement | null>(null);
     const ui = useRef<HTMLDivElement | null>(null);
 
-    const {camera, scene, system, renderer, world, keyPressed, isReady, screenSize, isStop, exit, init, stop} = useGame({container: container.current!, ui: ui.current!});
+    const { camera, scene, system, renderer, world, keyPressed, isReady, screenSize, isStop, exit, init, stop } = useGame({ container: container.current!, ui: ui.current! });
 
-    useEffect(() =>{
+    useEffect(() => {
         if (container.current && ui.current)
             init(true);
     }, [container.current, ui.current])
 
     // add script here
     const counted = useRef<boolean>(false);
-    useEffect(() =>{
+    useEffect(() => {
         if (counted.current)
             return;
         if (!isReady)
             return;
-        counted.current = true; 
+        counted.current = true;
 
         let ground = createEntity('ground');
-        insertComponent(ground, {id: 'transform', rotate_x: 0});
+        insertComponent(ground, { id: 'transform' });
         insertComponent(ground, {
-            id: 'circle_plane',
-            radius: 1,
-            segments: 16,
-            color: 0xdae1ed
+            id: 'model',
+            bucket: 'scenes',
+            file: 'GameMap/arena.glb',
+            scale: { x: 0.35, y: 0.35, z: 0.35 }
+        });
+        insertComponent(ground, {
+            id: 'hitbox',
+            width: 10,
+            height: 0.1,
+            depth: 10
         });
         insertEntityToSystem(ground, system, scene, world, ui.current!);
 
@@ -51,12 +57,12 @@ export function Game(){
     }, [isReady])
 
     const room = useRef<RealtimeChannel | null>(null);
-    useEffect(() =>{
+    useEffect(() => {
         if (!renderer)
             return;
         if (!isReady)
             return;
-        
+
         if (isStop)
             renderer.setAnimationLoop(null);
         else
@@ -64,7 +70,7 @@ export function Game(){
     }, [isReady, scene, world, renderer, system, keyPressed, camera, screenSize, isStop])
 
     const [players, setPlayers] = useState<Record<string, any>>({});
-    useEffect(() =>{
+    useEffect(() => {
         if (!isReady)
             return;
         if (!id)
@@ -82,110 +88,110 @@ export function Game(){
         });
 
         room.current
-        .on('presence', { event: 'sync' }, async() => {
-            const new_state = room.current!.presenceState();
-            let dict: typeof players = {};
-            let new_state_dict = Object.entries(new_state);
-            new_state_dict.forEach(([client, data]) =>{
-                dict[client] = data[0];
-                setPlayers(dict);
+            .on('presence', { event: 'sync' }, async () => {
+                const new_state = room.current!.presenceState();
+                let dict: typeof players = {};
+                let new_state_dict = Object.entries(new_state);
+                new_state_dict.forEach(([client, data]) => {
+                    dict[client] = data[0];
+                    setPlayers(dict);
+                })
             })
-        })
-        .on('presence', { event: 'join' }, async ({ key, newPresences }) => {
-            stop(true);
-            let player = createEntity(key);
-            insertComponent(player, {id: 'transform', y: 0.5});
-            insertComponent(player, {
-                id: 'model',
-                bucket: 'characters',
-                file: `players/${newPresences[0].skin}`,
-                scale: {x: 0.001, y: 0.001, z: 0.001}
+            .on('presence', { event: 'join' }, async ({ key, newPresences }) => {
+                stop(true);
+                let player = createEntity(key);
+                insertComponent(player, { id: 'transform', y: 0.5 });
+                insertComponent(player, {
+                    id: 'model',
+                    bucket: 'characters',
+                    file: `players/${newPresences[0].skin}`,
+                    scale: { x: 0.001, y: 0.001, z: 0.001 }
+                })
+                insertComponent(player, {
+                    id: 'hitbox',
+                    width: 0.1,
+                    height: 0.1,
+                    depth: 0.1
+                });
+                insertComponent(player, {
+                    id: 'text',
+                    text: newPresences[0].username,
+                    y: 0.22,
+                    size: 12,
+                    color: '#ffffff'
+                });
+                if (key === account.user_id) {
+                    insertComponent(player, { id: 'physic', static: true });
+                    insertComponent(player, { id: 'controller2' });
+                    insertComponent(player, { id: 'camera' });
+                    insertComponent(player, { id: 'sync' });
+                }
+                await insertEntityToSystem(player, system, scene, world, ui.current!);
+                stop(false);
             })
-            insertComponent(player, {
-                id: 'hitbox',
-                width: 0.1,
-                height: 0.1,
-                depth: 0.1
-            });
-            insertComponent(player, {
-                id: 'text',
-                text: newPresences[0].username,
-                y: 0.22,
-                size: 12,
-                color: '#ffffff'
-            });
-            if (key === account.user_id){
-                insertComponent(player, {id: 'physic', static: true});
-                insertComponent(player, {id: 'controller2'});
-                insertComponent(player, {id: 'camera'});
-                insertComponent(player, {id: 'sync'});
-            }
-            await insertEntityToSystem(player, system, scene, world, ui.current!);
-            stop(false);
-        })
-        .on('presence', { event: 'leave' }, ({ key, leftPresences }) => {
-            let entity = system[key];
-            if (!entity)
-                return;
-            scene.remove(entity.gameObject.model);
-            world.removeBody(entity.gameObject.hitbox);
-            if (entity.gameObject.text){
-                entity.gameObject.text.remove();
-            }
-            delete system[key];
-        })
-        .on(
-            'broadcast',
-            { event: 't' },
-            (data) => {
-                let entity_id = data.payload.id;
-                if (entity_id === account.user_id)
-                    return;
-                let entity = system[entity_id];
+            .on('presence', { event: 'leave' }, ({ key, leftPresences }) => {
+                let entity = system[key];
                 if (!entity)
                     return;
-                let transform = data.payload.transform;
-                let self_transform = entity.components['transform'];
-                let hitbox = system[entity_id].gameObject.hitbox as CANNON.Body;
-                hitbox.position.set(transform.position.x, transform.position.y, transform.position.z);
-                self_transform.x = transform.position.x;
-                self_transform.y = transform.position.y;
-                self_transform.z = transform.position.z;
-                self_transform.rotate_x = transform.rotation.x;
-                self_transform.rotate_y = transform.rotation.y;
-                self_transform.rotate_z = transform.rotation.z;
-                self_transform.scale = transform.scale;
-            }
-        )
-        .on(
-            'broadcast',
-            { event: 'tr' },
-            (data) => {
-                let entity_id = data.payload.id;
-                if (entity_id === account.user_id)
+                scene.remove(entity.gameObject.model);
+                world.removeBody(entity.gameObject.hitbox);
+                if (entity.gameObject.text) {
+                    entity.gameObject.text.remove();
+                }
+                delete system[key];
+            })
+            .on(
+                'broadcast',
+                { event: 't' },
+                (data) => {
+                    let entity_id = data.payload.id;
+                    if (entity_id === account.user_id)
+                        return;
+                    let entity = system[entity_id];
+                    if (!entity)
+                        return;
+                    let transform = data.payload.transform;
+                    let self_transform = entity.components['transform'];
+                    let hitbox = system[entity_id].gameObject.hitbox as CANNON.Body;
+                    hitbox.position.set(transform.position.x, transform.position.y, transform.position.z);
+                    self_transform.x = transform.position.x;
+                    self_transform.y = transform.position.y;
+                    self_transform.z = transform.position.z;
+                    self_transform.rotate_x = transform.rotation.x;
+                    self_transform.rotate_y = transform.rotation.y;
+                    self_transform.rotate_z = transform.rotation.z;
+                    self_transform.scale = transform.scale;
+                }
+            )
+            .on(
+                'broadcast',
+                { event: 'tr' },
+                (data) => {
+                    let entity_id = data.payload.id;
+                    if (entity_id === account.user_id)
+                        return;
+                    let entity = system[entity_id];
+                    if (!entity)
+                        return;
+                    entity.components['transform'].time_rotate = 0;
+                }
+            )
+            .subscribe(async (status) => {
+                if (status !== 'SUBSCRIBED')
                     return;
-                let entity = system[entity_id];
-                if (!entity)
-                    return;
-                entity.components['transform'].time_rotate = 0;
-            }
-        )
-        .subscribe( async(status) =>{
-            if (status !== 'SUBSCRIBED')
-                return;
 
-            // initial
-            let _skin = skin;
-            if (!_skin)
-                _skin = 'knight3.glb';
-            await room.current!.track({
-                username: account.username,
-                skin: _skin
+                // initial
+                let _skin = skin;
+                if (!_skin)
+                    _skin = 'knight3.glb';
+                await room.current!.track({
+                    username: account.username,
+                    skin: _skin
+                });
             });
-        });
 
-        return () =>{
-            if (room.current){
+        return () => {
+            if (room.current) {
                 room.current.untrack();
                 room.current.unsubscribe();
             }
@@ -194,7 +200,7 @@ export function Game(){
 
     return (
         <div className=" relative w-full h-full bg-[#84a6c9] flex justify-center items-center">
-            
+
             <div className='z-20 w-full h-full flex justify-start items-start pointer-events-none p-2'>
 
             </div>
